61143eb718062dd284e1552546ae21d437b36c8b
kunal <kunalsingh5431@gmail.com>
2025-05-18 09:58:11 +0530

    build update commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path"); 
 const authRoutes = require("./routes/authRoutes");
 const foodRoutes = require("./routes/foodRoutes");
 const orderRoutes = require("./routes/orderRoutes");
